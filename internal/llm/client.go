d819015c31938be6189566f0bb3723908157af64
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2026-04-08 18:58:34 +0530

    implement streaming

--- internal/llm/client.go
+++ internal/llm/client.go
@@ -8,17 +8,26 @@ import (
 
 func GetAnswer(prompt string) {
 	client := openai.NewClient()
+	ctx := context.Background()
 
-	chatCompletion, err := client.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
+	stream := client.Chat.Completions.NewStreaming(ctx, openai.ChatCompletionNewParams{
 		Messages: []openai.ChatCompletionMessageParamUnion{
 			openai.SystemMessage("You are Rivet. A terminal based minimal coding agent"),
 			openai.UserMessage(prompt),
 		},
 		Model: openai.ChatModelGPT5_2,
 	})
-	if err != nil {
-		panic(err)
+
+	for stream.Next() {
+		evt := stream.Current()
+		if len(evt.Choices) > 0 {
+			print(evt.Choices[0].Delta.Content)
+		}
+	}
+	println()
+
+	if err := stream.Err(); err != nil {
+		panic(err.Error())
 	}
 
-	println(chatCompletion.Choices[0].Message.Content)
 }
